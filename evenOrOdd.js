4046599d6169b90921b4211258f10b2856162721
Amanda <mrs.acapella@gmail.com>
2017-06-30 11:04:55 -0700

    finished with EC6 and optimization

---
+++ evenOrOdd.js
@@ -0,0 +1,13 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+const evenOrOdd = (n) => (n % 2 == 1) ? "Odd" : "Even";
+
+// var evenOrOdd = function(n){
+//    if(n % 2 == 1){
+//       return "Odd";
+//    } else {
+//       return "Even";
+//    }
+// }
+
+console.log(evenOrOdd(3));
