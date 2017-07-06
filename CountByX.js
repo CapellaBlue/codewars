43c9bf2bf32123ba06dca4ba4996ea41b2118fc7
Amanda <mrs.acapella@gmail.com>
2017-07-06 13:43:25 -0700

    countBy() first attempt

--- CountByX.js
+++ CountByX.js
@@ -8,3 +8,12 @@
 //
 // countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 // countBy(2,5) === [2,4,6,8,10]
+
+var countBy = function(x, n){
+   arr = [];
+   for (var i = x; i <= n; i*x) {
+      arr.push(i);
+   };
+};
+
+console.log(countBy(1,10));
